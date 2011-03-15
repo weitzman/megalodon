b7a2ede0be003f93cc988616ef8357a14d3a534c
Mark Sonnabaum <mark@sonnabaum.com>
2011-03-15 15:27:42 -0500

    Got the varnish recipe/template working for osx.

--- cookbooks/varnish/attributes/default.rb
+++ cookbooks/varnish/attributes/default.rb
@@ -1,1 +1,2 @@
 default[:varnish][:backend_hosts] = []
+default[:varnish][:backend_default] = {:host => "127.0.0.1", :port => 80}
--- cookbooks/varnish/recipes/default.rb
+++ cookbooks/varnish/recipes/default.rb
@@ -3,26 +3,17 @@
 
 homebrew "varnish"
 
-template "#{node[:varnish][:dir]}/default.vcl" do
+template "/usr/local/etc/varnish/default.vcl" do
   source "default.vcl.erb"
-  owner "root"
-  group "root"
+  #owner "root"
+  #group "root"
   mode 0644
 end
 
-template "#{node[:varnish][:default]}" do
-  source "ubuntu-default.erb"
-  owner "root"
-  group "root"
-  mode 0644
-end
-
-service "varnish" do
-  supports :restart => true, :reload => true
-  action [ :enable, :start ]
-end
-
-service "varnishlog" do
-  supports :restart => true, :reload => true
-  action [ :enable, :start ]
-end
+#template "#{node[:varnish][:default]}" do
+#  source "ubuntu-default.erb"
+#  owner "root"
+#  group "root"
+#  mode 0644
+#end
+#
