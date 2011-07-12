1b828ab74c74ee3d4d3d36b4a0703cc63cde18ed
Mark Sonnabaum <mark@sonnabaum.com>
2011-07-12 17:32:53 -0500

    Removed some stuff from the default recipe, no need to install homebrew.

--- cookbooks/homebrew/recipes/default.rb
+++ cookbooks/homebrew/recipes/default.rb
@@ -9,30 +9,8 @@ require root + '/resources/homebrew'
 require root + '/providers/homebrew'
 require 'etc'
 
-#directory "#{ENV['HOME']}/Developer" do
-  #action :create
-#end
-#
 directory "#{ENV['HOME']}/.megalodon" do
   action :create
-end
-
-execute "download homebrew installer" do
-  command "/usr/bin/curl -sfL http://github.com/mxcl/homebrew/tarball/master | /usr/bin/tar xz -m --strip 1"
-  #cwd     "#{ENV['HOME']}/Developer"
-  not_if  "test -e ~/usr/local/bin/brew"
-end
-
-script "install_something" do
-  interpreter "bash"
-  code <<-EOS
-  if [ -f ~/.cider.profile ]; then
-    rm ~/.cider.profile
-  fi
-  if [ -f ~/.cider.profile.custom ]; then
-    mv ~/.cider.profile.custom ~/.megalodon.profile.custom
-  fi
-  EOS
 end
 
 template "#{ENV['HOME']}/.megalodon.profile" do
