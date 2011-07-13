7d82cc3f9df928e0d67b773456ba510c15337f8d
Mark Sonnabaum <mark@sonnabaum.com>
2011-07-12 22:03:20 -0500

    blah

--- cookbooks/mvim/recipes/default.rb
+++ cookbooks/mvim/recipes/default.rb
@@ -6,7 +6,7 @@
 script "installing http://github.com/carlhuda/janus" do
   interpreter "bash"
   code <<-EOS
-    source ~/.cinderella.profile
+    source ~/.megaladon.profile
     if [ ! -d ~/.vim ]; then
       git clone git://github.com/carlhuda/janus.git ~/.vim
       cd ~/.vim
@@ -32,15 +32,15 @@ template "#{ENV['HOME']}/.gvimrc.local" do
   source "dot.vimrc.erb"
 end
 
-script "installed macvim from google code" do
+script "installed macvim from github" do
   interpreter "bash"
   code <<-EOS
-    source ~/.cinderella.profile
-    if [ ! -e ~/Developer/bin/mvim ]; then
+    source ~/.megaladon.profile
+    if [ ! -e /usr/local/bin/mvim ]; then
       rm -rf /Applications/MacVim.app
-      curl -L http://github.com/downloads/b4winckler/macvim/MacVim-snapshot-55.tbz -o - | tar xj -
-      cd MacVim-snapshot-55
-      cp mvim ~/Developer/bin
+      curl -L https://github.com/downloads/b4winckler/macvim/MacVim-snapshot-59.tbz -o - | tar xj -
+      cd MacVim-snapshot-59
+      cp mvim /usr/local/bin/
       cp -r MacVim.app /Applications/
     fi
   EOS
