b69be77889ecdb5bc00315a665aadfc13d2450af
Mark Sonnabaum <mark@sonnabaum.com>
2011-07-12 22:03:36 -0500

    Updating info on phpunit cookbook.

--- cookbooks/phpunit/recipes/default.rb
+++ cookbooks/phpunit/recipes/default.rb
@@ -1,8 +1,6 @@
-#
-# Cookbook Name::phpunit
-# Recipe:: source
-#
-# Copyright 2010, Opscode, Inc.
+# Author:: Mark Sonnabaum <mark.sonnabaum@acquia.com>
+# Cookbook Name::  phpunit
+# Recipe:: default
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 include_recipe "php"
 
