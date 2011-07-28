a6da89662ddfe7cbec480f38218dce5ae17c2c53
Mark Sonnabaum <mark@sonnabaum.com>
2011-07-28 14:18:56 -0500

    Added enabling php automatically in the apache conf.

--- cookbooks/php/recipes/default.rb
+++ cookbooks/php/recipes/default.rb
@@ -16,6 +16,13 @@ homebrew "php" do
 
 end
 
+bash "Add php to apache conf" do
+  code <<-EOS
+  sudo sed -i -e "s%^#LoadModule php5_module libexec/apache2/libphp5.so%LoadModule php5_module $(brew --prefix php)/libexec/apache2/libphp5.so%" /etc/apache2/httpd.conf
+  sudo sed -i -e "s%^#Include /private/etc/apache2/extra/httpd-vhosts.conf%Include /private/etc/apache2/extra/httpd-vhosts.conf%" /etc/apache2/httpd.conf
+  EOS
+end
+
 bash "Fix the default PEAR permissions and config" do
   code <<-EOS
     chmod -R ug+w $(brew --prefix)/lib/php
