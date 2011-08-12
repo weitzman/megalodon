18d6bece7b8b7100545ee56548d5405e0d035c0f
Mark Sonnabaum <mark@sonnabaum.com>
2011-08-12 17:05:52 -0500

    Trying a hack to get php-cgi working with both fpm and apache. Also some fpm fixes.

--- cookbooks/php/recipes/fpm.rb
+++ cookbooks/php/recipes/fpm.rb
@@ -6,17 +6,21 @@
 
 package "php" do
   action :install
-  options "--with-mysql --with-pgsql --with-mssql --with-imap --with-fpm"
+  options "--with-mysql --with-pgsql --with-mssql --with-imap --with-fpm --with-cgi"
 end
 
 bash "Fix the default PEAR permissions and config" do
   code <<-EOS
-    chmod -R ug+w $(brew --prefix)/lib/php
+    chmod -R ug+w $(brew --prefix php)/lib/php
     pear config-set php_ini $(brew --prefix)/etc/php5/php.ini
   EOS
 end
 
-directory node[:php][:scan_dir] do
+directory node['php']['scan_dir'] do
+  action :create
+end
+
+directory "#{node['php']['conf_dir']}/fpm" do
   action :create
 end
 
@@ -30,7 +34,7 @@ template "#{node['php']['conf_dir']}/php.ini" do
   mode "0644"
 end
 
-template "#{node['php']['conf_dir']}/php-fpm.conf" do
+template "#{node['php']['conf_dir']}/fpm/php-fpm.conf" do
   source "php-fpm.conf.erb"
   mode "0644"
 end
