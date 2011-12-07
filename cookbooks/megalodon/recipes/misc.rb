28c33008a413d5f24369c57c5e4c376cd661b0ba
Mark Sonnabaum <mark@sonnabaum.com>
2011-12-07 15:39:35 -0600

    Removing some more stuff that should be optional.

--- cookbooks/megalodon/recipes/misc.rb
+++ cookbooks/megalodon/recipes/misc.rb
@@ -9,6 +9,6 @@
 #require root + '/providers/homebrew'
 include_recipe 'homebrew'
 
-%w(bash-completion tig ack coreutils sqlite gist wget hub fortune proctools markdown ctags).each do |pkg|
+%w(bash-completion coreutils sqlite wget markdown ctags).each do |pkg|
   package pkg
 end
