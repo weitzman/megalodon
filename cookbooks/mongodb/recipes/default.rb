a9ec2645b3a516041c4292938e9dfd61860588b9
Mark Sonnabaum <mark@sonnabaum.com>
2012-04-11 23:15:57 -0500

    Fixed plist path in mongo cookbook.

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -3,19 +3,21 @@
 
 package "mongodb"
 
+plist="homebrew.mxcl.mongodb.plist"
+
 bash "copy plist firsttime" do
   code <<-EOS
-    cp /usr/local/Cellar/mongodb/*/org.mongodb.mongod.plist ~/Library/LaunchAgents/
-    launchctl load -w ~/Library/LaunchAgents/org.mongodb.mongod.plist
+  (cp /usr/local/Cellar/mongodb/*/#{plist} ~/Library/LaunchAgents/)
+  (launchctl load -w ~/Library/LaunchAgents/#{plist})
   EOS
-  not_if File.exists?("~/Library/LaunchAgents/org.mongodb.mongod.plist")
+  not_if File.exists?("~/Library/LaunchAgents/#{plist}")
 end
 
 bash "copy new plist" do
   code <<-EOS
-    launchctl unload -w ~/Library/LaunchAgents/org.mongodb.mongod.plist
-    cp /usr/local/Cellar/mongodb/*/org.mongodb.mongod.plist ~/Library/LaunchAgents/
-    launchctl load -w ~/Library/LaunchAgents/org.mongodb.mongod.plist
+    launchctl unload -w ~/Library/LaunchAgents/#{plist}
+    cp /usr/local/Cellar/mongodb/*/#{plist} ~/Library/LaunchAgents/
+    launchctl load -w ~/Library/LaunchAgents/#{plist}
   EOS
-  only_if File.exists?("~/Library/LaunchAgents/org.mongodb.mongod.plist")
+  only_if File.exists?("~/Library/LaunchAgents/#{plist}")
 end
