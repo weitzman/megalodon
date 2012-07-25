ff1a82bd5251ce7dc05bab139281bfcaf28bede0
Mark Sonnabaum <mark@sonnabaum.com>
2012-07-25 12:11:40 -0400

    Fixed a bug determining the php version output.
    Change in the output of brew info broke this.

--- cookbooks/megalodon/attributes/default.rb
+++ cookbooks/megalodon/attributes/default.rb
@@ -1,3 +1,3 @@
 default['megalodon']['brew_prefix'] = `brew --prefix`.strip
-default['megalodon']['php_version'] = `brew info php| head -1 | awk '{print $2}'`.strip
+default['megalodon']['php_version'] = `brew --prefix php`.strip.split('/').last
 default['megalodon']['docroot'] = "#{default['megalodon']['brew_prefix']}/var/www"
