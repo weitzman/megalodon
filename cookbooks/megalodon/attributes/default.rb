ff29d980ecb88c779e50dc21ab579d7724227ccb
Mark Sonnabaum <mark@sonnabaum.com>
2012-10-23 17:01:06 -0500

    Fixed a php path bug introduced in a homebrew change.
    Also disabled an xdebug parameter that was annoying.

--- cookbooks/megalodon/attributes/default.rb
+++ cookbooks/megalodon/attributes/default.rb
@@ -1,3 +1,3 @@
 default['megalodon']['brew_prefix'] = `brew --prefix`.strip
-default['megalodon']['php_version'] = `brew --prefix php`.strip.split('/').last
+default['megalodon']['php_prefix'] = `brew --prefix php`.strip
 default['megalodon']['docroot'] = "#{default['megalodon']['brew_prefix']}/var/www"
